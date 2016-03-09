f131519f77f04d366b71c5d3d98677cb33fcd50d
Cédric Verstraeten <hello@cedric.ws>
2016-03-09 19:02:46 +0100

    add dependencies

--- package/kerberosio-machinery/kerberosio-machinery.mk
+++ package/kerberosio-machinery/kerberosio-machinery.mk
@@ -1,13 +1,14 @@
-#############################################################
+############################################################
 #
 # Kerberos.io - Machinery
 #
 #############################################################
+
 KERBEROSIO_MACHINERY_VERSION = develop
 KERBEROSIO_MACHINERY_SITE = https://github.com/kerberos-io/machinery
 KERBEROSIO_MACHINERY_SITE_METHOD = git
 KERBEROSIO_MACHINERY_INSTALL_TARGET = YES
-KERBEROSIO_MACHINERY_DEPENDENCIES = libcurl
+KERBEROSIO_MACHINERY_DEPENDENCIES = libcurl ffmpeg rpi-userland rpi-firmware rpi-armmem
 KERBEROSIO_MACHINERY_MAKE=$(MAKE1)
 
 $(eval $(cmake-package))
