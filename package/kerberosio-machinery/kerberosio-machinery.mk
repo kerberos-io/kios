910d0f14e2367958f5f40260d3f1d0933d00678f
Cédric Verstraeten <hello@cedric.ws>
2016-03-11 14:20:56 +0100

    add startup scripts for machinery

--- package/kerberosio-machinery/kerberosio-machinery.mk
+++ package/kerberosio-machinery/kerberosio-machinery.mk
@@ -11,4 +11,19 @@ KERBEROSIO_MACHINERY_INSTALL_TARGET = YES
 KERBEROSIO_MACHINERY_DEPENDENCIES = libcurl ffmpeg rpi-userland rpi-firmware rpi-armmem
 KERBEROSIO_MACHINERY_MAKE=$(MAKE1)
 
+define KERBEROSIO_MACHINERY_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 package/kerberosio-machinery/kerberosio.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/kerberosio.service
+
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+
+	ln -fs ../../../../usr/lib/systemd/system/kerberosio.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/kerberosio.service
+endef
+
+define KERBEROSIO_MACHINERY_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 package/kerberosio-machinery/S99kerberosio \
+		$(TARGET_DIR)/etc/init.d/S99kerberosio
+endef
+
 $(eval $(cmake-package))
