11024436dfa8d6ca7e6decad0cd8c027e3c1f955
Cédric Verstraeten <hello@cedric.ws>
2016-04-01 11:55:32 +0200

    add cpack  (debian package) to post install

--- package/kerberosio-machinery/kerberosio-machinery.mk
+++ package/kerberosio-machinery/kerberosio-machinery.mk
@@ -25,6 +25,11 @@ KERBEROSIO_MACHINERY_PRE_INSTALL_TARGET_HOOKS += KERBEROSIO_MACHINERY_REMOVE_INS
 
 define KERBEROSIO_MACHINERY_BIND_DIRS_TO_DATA
 
+    #create .deb package
+    (cd $(@D); \
+        $(HOST_DIR)/usr/bin/cpack; \
+    )
+    
 	# Link directories to data folder
 	rm -rf $(TARGET_DIR)/etc/opt/kerberosio/capture
 	ln -s /data/machinery/capture $(TARGET_DIR)/etc/opt/kerberosio/capture 
