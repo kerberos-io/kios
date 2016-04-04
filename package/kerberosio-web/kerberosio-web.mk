cee99d8e7544c59e4990d6ca217ffbfa8b4247f7
Cédric Verstraeten <hello@cedric.ws>
2016-04-04 16:55:29 +0200

    add composer install

--- package/kerberosio-web/kerberosio-web.mk
+++ package/kerberosio-web/kerberosio-web.mk
@@ -22,6 +22,7 @@ define KERBEROSIO_WEB_BUILD_CMDS
         php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"; \
         php composer-setup.php; \
         php -r "unlink('composer-setup.php');"; \
+        php composer.phar install; \
         cd public; \
         bower install --allow-root; \
     )
