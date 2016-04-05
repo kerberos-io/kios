77fc0193da15a290a12985c7cb12627093099338
Cédric Verstraeten <hello@cedric.ws>
2016-04-05 13:00:35 +0200

    add php-pod

--- install-centos.sh
+++ install-centos.sh
@@ -3,7 +3,7 @@
 yum install -y git hg bc perl-devel 
 curl --silent --location https://rpm.nodesource.com/setup_5.x | bash -
 yum install -y php  php-xml php-gd php-curl nodejs
-yum -y install epel-release && yum -y install php-mcrypt
+yum -y install epel-release && yum -y install php-mcrypt php-pdo
 npm install -g bower
 yum group install "Development Tools" -y
 git clone https://github.com/kerberos-io/kios
