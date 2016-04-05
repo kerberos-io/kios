62b11829e86341602612547277f35ad694cf0a8e
Cédric Verstraeten <hello@cedric.ws>
2016-04-05 12:15:19 +0200

    add perl-revel

--- install-centos.sh
+++ install-centos.sh
@@ -1,7 +1,6 @@
-yum install -y git
+yum install -y git hg bc perl-devel
 git clone https://github.com/kerberos-io/kios
 cd kios
 git checkout develop
 yum group install "Development Tools" -y
-yum install hg bc -y
 /root/kios/build.sh all
