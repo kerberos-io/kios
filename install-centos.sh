3712ec3bca13413b9ce9d12038cd82a4b9aad1b7
Cédric Verstraeten <hello@cedric.ws>
2016-04-05 12:51:42 +0200

    add php extensions to install script

--- install-centos.sh
+++ install-centos.sh
@@ -1,6 +1,6 @@
 yum install -y git hg bc perl-devel 
 curl --silent --location https://rpm.nodesource.com/setup_5.x | bash -
-yum install -y php nodejs
+yum install -y php  php-xml php-gd php-curl nodejs
 npm install -g bower
 yum group install "Development Tools" -y
 git clone https://github.com/kerberos-io/kios
