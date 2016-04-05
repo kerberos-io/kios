8005bc07e09329ee1e1129ca8fa7e709aacee7a1
Cédric Verstraeten <hello@cedric.ws>
2016-04-05 12:55:05 +0200

    add php-crypt

--- install-centos.sh
+++ install-centos.sh
@@ -1,6 +1,9 @@
+#CentOS 7
+
 yum install -y git hg bc perl-devel 
 curl --silent --location https://rpm.nodesource.com/setup_5.x | bash -
 yum install -y php  php-xml php-gd php-curl nodejs
+yum -y install epel-release && yum -y install php-mcrypt
 npm install -g bower
 yum group install "Development Tools" -y
 git clone https://github.com/kerberos-io/kios
