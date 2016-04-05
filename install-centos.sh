864e50af500480c51f60998f6101715aa617a5d3
Cédric Verstraeten <hello@cedric.ws>
2016-04-05 12:44:21 +0200

    add php and bower

--- install-centos.sh
+++ install-centos.sh
@@ -1,6 +1,9 @@
-yum install -y git hg bc perl-devel
+yum install -y git hg bc perl-devel 
+curl --silent --location https://rpm.nodesource.com/setup_5.x | bash -
+yum install -y php nodejs
+npm install -g bower
+yum group install "Development Tools" -y
 git clone https://github.com/kerberos-io/kios
 cd kios
 git checkout develop
-yum group install "Development Tools" -y
 /root/kios/build.sh all
