5c2476a236d731854a78e6b4764a6cf6afb8f7c7
Cédric Verstraeten <hello@cedric.ws>
2016-04-28 23:17:53 +0200

    remove hash check, can change..

--- package/kerberosio-web/kerberosio-web.mk
+++ package/kerberosio-web/kerberosio-web.mk
@@ -19,7 +19,6 @@ KERBEROSIO_WEB_DEPENDENCIES = kerberosio-machinery nginx php
 define KERBEROSIO_WEB_BUILD_CMDS
     (cd $(@D); \
         php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php; \
-        php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"; \
         php composer-setup.php; \
         php -r "unlink('composer-setup.php');"; \
         php composer.phar install; \
