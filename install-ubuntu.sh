9444bf9bca5bf1dd1928c2f2dd3927d990bba4c6
Cédric Verstraeten <cedric.verstraeten@me.com>
2016-06-08 11:21:02 +0200

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y git build-essential unzip mercurial php5 php5-gd mcrypt php5-mcrypt php5-curl
 php5enmod mcrypt
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-apt-get install -y nodejs npm
+apt-get install -y nodejs npm nodejs-legacy
 npm install -g bower
 git clone https://github.com/kerberos-io/kios
 cd kios
