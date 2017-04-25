dcc2d2ced8d5f10d49483f2a829604038a3f9bdb
Cédric Verstraeten <hello@cedric.ws>
2017-04-25 21:26:26 +0200

    update firmware and user land

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 2350bf2511fa49e177fb35c9613eef1b657a7506
+RPI_USERLAND_VERSION = cdb5da59f939eb4078e90ed0e3c231c498ba9957
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILES = LICENCE
